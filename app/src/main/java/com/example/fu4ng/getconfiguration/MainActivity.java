11ab43a1202d348f388a772bc5bb6d0621356128
Fu4ng <903838834@qq.com>
2018-05-05 15:26:56 +0800

    f

---
+++ app/src/main/java/com/example/fu4ng/getconfiguration/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.fu4ng.getconfiguration;
+
+import android.app.Activity;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+import android.widget.TextView;
+
+import com.example.fu4ng.getconfiguration.R;
+
+
+public class MainActivity extends Activity
+{
+    TextView ori;
+    TextView nav;
+    TextView tou;
+    TextView mnc;
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // 获取应用界面中的界面组件
+        ori = (TextView) findViewById(R.id.ori);
+        nav = (TextView) findViewById(R.id.nav);
+        tou= (TextView) findViewById(R.id.tou);
+        mnc = (TextView) findViewById(R.id.mnc);
+        Button bn = (Button)findViewById(R.id.bn);
+        bn.setOnClickListener(new OnClickListener()
+        {
+            // 为按钮绑定事件监听器
+            @Override
+            public void onClick(View source)
+            {
+                // 获取系统的Configuration对象
+                Configuration cfg = getResources().getConfiguration();
+                String screen = cfg.orientation ==
+                        Configuration.ORIENTATION_LANDSCAPE
+                        ? "横向屏幕": "竖向屏幕";
+                String mncCode = cfg.mnc + "";
+                String naviName = cfg.orientation ==
+                        Configuration.NAVIGATION_NONAV
+                        ? "没有方向控制" :
+                        cfg.orientation == Configuration.NAVIGATION_WHEEL
+                                ? "滚轮控制方向" :
+                                cfg.orientation == Configuration.NAVIGATION_DPAD
+                                        ? "方向键控制方向" : "轨迹球控制方向";
+                nav.setText(naviName);
+                String touchName = cfg.touchscreen ==
+                        Configuration.TOUCHSCREEN_NOTOUCH
+                        ? "无触摸屏" : "支持触摸屏";
+                ori.setText(screen);
+                mnc.setText(mncCode);
+                tou.setText(touchName);
+            }
+        });
+    }
+}
+
